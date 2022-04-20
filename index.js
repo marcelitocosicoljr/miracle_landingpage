f23333378828b0605bf2a9af1830e064259cf3e0
Marcelito Cosicol <78013167+marcelitocosicoljr@users.noreply.github.com>
2022-04-20 10:07:28 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function showVideo() {
+    var element = document.getElementById("popup");
+    element.classList.add("active");
+}
+
+ function hideVideo() {
+    var element = document.getElementById("popup");
+    element.classList.remove("active");
+}
+ 
